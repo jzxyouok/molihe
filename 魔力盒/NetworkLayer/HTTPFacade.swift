03b62d8720eca62a9a79d1265e3ceea2ac2523cb
litianpeng <litianpeng@zuoyebang.com>
2017-06-18 14:44:08 +0800

    test

--- 魔力盒/NetworkLayer/HTTPFacade.swift
+++ 魔力盒/NetworkLayer/HTTPFacade.swift
@@ -9,35 +9,35 @@
 import Foundation
 import Alamofire
 import SwiftyJSON
-//struct HTTPFacade {
-//    static let shareInstance = HTTPFacade()
-//    func loadMagicTVData(_ finished:@escaping(_ response:DataResponse<Any>) -> ())  {
-//        //https://magicapi.vmovier.cc/magicapiv2/find/magictv
-//            Alamofire.request(URL(string: "https://magicapi.vmovier.cc/magicapiv2/find/magictv")!, method: .post, parameters: nil, encoding: JSONEncoding.default, headers: [:]).responseJSON(completionHandler: { (response) in
-//                finished(response)
-//            })
-//     }
-//}
+struct HTTPFacade {
+    static let shareInstance = HTTPFacade()
+    func loadMagicTVData(_ finished:@escaping(_ response:DataResponse<Any>) -> ())  {
+        //https://magicapi.vmovier.cc/magicapiv2/find/magictv
+            Alamofire.request(URL(string: "https://magicapi.vmovier.cc/magicapiv2/find/magictv")!, method: .post, parameters: nil, encoding: JSONEncoding.default, headers: [:]).responseJSON(completionHandler: { (response) in
+                finished(response)
+            })
+     }
+}
 
 
 
+//
+// typealias CompleteBlock = (_ error:NSError?, _ resultObejct:Any?) -> ()
+// struct HTTPFacade {
+// static let shareInstance = HTTPFacade()
+// 
+// 
+// func loadMagicTVData(_ finished:@escaping CompleteBlock)  {
+// 
+//         Alamofire.request(URL(string: findmagictv)!, method: .post, parameters: nil, encoding: JSONEncoding.default, headers: [:]).responseJSON(completionHandler: { (response) in
+//         finished(NSError(),response)
+//         })
+// }
+// 
+// 
+// private func doHttpRequest(params:[String:Any] , url:String){
+// 
+// }
+// 
+// }
 
- typealias CompleteBlock = (_ error:NSError?, _ resultObejct:Any?) -> ()
- struct HTTPFacade {
- static let shareInstance = HTTPFacade()
- 
- 
- func loadMagicTVData(_ finished:@escaping CompleteBlock)  {
- 
-         Alamofire.request(URL(string: findmagictv)!, method: .post, parameters: nil, encoding: JSONEncoding.default, headers: [:]).responseJSON(completionHandler: { (response) in
-         finished(NSError(),response)
-         })
- }
- 
- 
- private func doHttpRequest(params:[String:Any] , url:String){
- 
- }
- 
- }
- 
